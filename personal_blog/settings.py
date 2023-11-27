e761ce2afc5c93e033ba73402162aa11f7b238bf
baambii <113696826+baambii@users.noreply.github.com>
2023-11-27 08:31:05 +0100

    settings.py

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'crispy_bootstrap4',
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
    
